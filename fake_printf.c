d3b3cec81c2d42a5d922bf2ddc4e9e0543ee7c94
vicco <mm.souichi@gmail.com>
2016-12-02 01:33:30 +0900

    change printf to show the address of argument

--- fake_printf.c
+++ fake_printf.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdarg.h>
+#define __USE_GNU
+#include <dlfcn.h>
 
 int printf(const char *fmt, ...){
-    puts("nya-n");
     char *ptr;
-    
+    void *handle;
     va_list ap;
+
+    puts("nya-n");
+
+    handle = dlsym(RTLD_NEXT, "printf");
+    int (*func)(const char *, ...) = (int (*)(const char*, ...))handle;
+
     va_start(ap, fmt);
     
     ptr = va_arg(ap, char*);
+    func("%p\n", ptr);
     while (ptr != NULL) {
         ptr = va_arg(ap, char*);
+        func("%p\n", ptr);
     }
-    
+
     va_end(ap);
-    
+
     return 0;
 }
