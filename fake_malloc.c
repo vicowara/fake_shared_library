edf6eabe800fa23927f8c35b8dd108baf069e350
vicowara <mm.souichi@gmail.com>
2016-12-02 14:38:27 +0900

    add functionality to malloc and free as normal one

--- fake_malloc.c
+++ fake_malloc.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
-void* malloc(size_t size) {
-    return NULL;
+void *malloc(size_t size) {
+    void *ptr;
+    void *handle;
+
+    handle = dlsym(RTLD_NEXT, "malloc");
+    void* (*original_malloc)(size_t) = (void* (*)(size_t))handle;
+
+    ptr = original_malloc(size);
+
+    // TODO: add ptr to list
+
+    return ptr;
 }
 
 void free(void *ptr) {
+    void *handle;
+
+    handle = dlsym(RTLD_NEXT, "free");
+    void (*original_free)(void*) = (void (*)(void*))handle;
+
+    // TODO: remove ptr from list
+
+    original_free(ptr);
 }
